5bf0b503d81146d2b3a090b0ec500bb65d42891a
Алексей <alexey@tood>
2015-12-25 16:20:21 +0500

    Server page

--- nodejs/server3008.js
+++ nodejs/server3008.js
@@ -1,19 +1,58 @@
-var io = require('socket.io')(3008);
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(3008);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+var calcDistance=function (j) {
+
+    var ratio_db = j.txCalibratedPower - j.rssi;
+    var ratio_linear = Math.pow(10, ratio_db / 10);
+
+    var r = Math.sqrt(ratio_linear);
+    j.distance=r;
+    return r;
+};//calcDistance
 
 io.on('connection', function (socket) {
   console.log('Connected');
-  socket.on('leText', function (leText) {
-    console.log('message ', leText);
+  socket.emit('info',{
+    mac:'test',
+    uuid:'testUUID',
+    rssi:-61
   });
 
   socket.on('bleData', function (json) {
+    var j=null;
     try{
       var j=JSON.parse(json);
     }catch(e){
     //can not parse
-      j={}
     }
-    console.log('bleData',JSON.stringify(j,null,'  '));
+    if (j){
+      j.uuid="";
+      j.scanRecord.slice(9,25).forEach(function(b){
+        j.uuid+=Number(b).toString(16);
+      });
+      j.txCalibratedPower=j.scanRecord[29];
+
+      calcDistance(j);
+      socket.broadcast.emit('info',j);
+    }
+
+    console.log('bleData',j);
   });
 
   socket.on('disconnect', function () {
